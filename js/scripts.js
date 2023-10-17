8bfd12adef935f8e38873b1f74ce3c528b37184e
DJFancyAl <abluemleiv@gmail.com>
2023-10-17 14:32:57 -0400

    Improved styling.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,27 @@ const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const text = document.body.textContent;
 
-// Disables the popover
+window.addEventListener('DOMContentLoaded', event => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+        return;
+    }
+    if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+        navbarCollapsible.classList.add('navbar-shrink')
+    }
 
+};
+
+// Shrink the navbar 
+navbarShrink();
+
+// Shrink the navbar when page is scrolled
+document.addEventListener('scroll', navbarShrink);
+})
 
 // Executes the search
 searchButton.addEventListener('click', () => {
